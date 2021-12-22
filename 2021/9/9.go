6426d048c5f5391ceb064c9be50c4c2651ba0319
Sean Shields <sean.t.shields@gmail.com>
2021-12-22 16:12:43 -0500

    day 9 part 2

--- 2021/9/9.go
+++ 2021/9/9.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"sort"
 	"strings"
 )
 
@@ -20,67 +20,119 @@ func main() {
 
 func solvePart1(input string) int {
 	rows := strings.Split(input, "\r\n")
-
+	low := findLowPoints(rows)
 	lowPoints := []int{}
-	for r, row := range rows {
-		for c, char := range row {
-			digit := int(char - '0')
-			top := ""
-			right := ""
-			bottom := ""
-			left := ""
-			adjacent := []int{}
-
-			if r != 0 {
-				row := rows[r-1]
-				for p, cc := range row {
-					if p == c {
-						top = string(cc)
-					}
-				}
-			}
-			if c < len(row)-1 {
-				right = string(row[c+1])
-			}
-			if r < len(rows)-1 {
-				row := rows[r+1]
-				for p, cc := range row {
-					if p == c {
-						bottom = string(cc)
-					}
-				}
-			}
-			if c != 0 {
-				left = string(row[c-1])
-			}
-			if top != "" {
-				t, _ := strconv.Atoi(top)
-				adjacent = append(adjacent, t)
-			}
-			if right != "" {
-				r, _ := strconv.Atoi(right)
-				adjacent = append(adjacent, r)
-			}
-			if bottom != "" {
-				b, _ := strconv.Atoi(bottom)
-				adjacent = append(adjacent, b)
-			}
-			if left != "" {
-				l, _ := strconv.Atoi(left)
-				adjacent = append(adjacent, l)
-			}
-
-			if isLow(adjacent, digit) {
-				lowPoints = append(lowPoints, digit)
-			}
-		}
+	for _, p := range low {
+		lowPoints = append(lowPoints, p.digit)
 	}
-
 	return sum(lowPoints) + len(lowPoints)
 }
 
-func solvePart2(input string) string {
-	return "Not Implemented"
+func solvePart2(input string) int {
+	rows := strings.Split(input, "\r\n")
+	lowPoints := findLowPoints(rows)
+	basins := [][]Point{}
+	for _, point := range lowPoints {
+		basin := Basin{point}
+		basin.calculate(rows, point)
+		basins = append(basins, basin)
+	}
+	sort.Slice(basins, func(i, j int) bool {
+		return len(basins[i]) > len(basins[j])
+	})
+	return len(basins[0]) * len(basins[1]) * len(basins[2])
+}
+
+type Basin []Point
+
+type Point struct {
+	digit       int
+	r           int
+	c           int
+	initialized bool
+}
+
+func findLowPoints(rows []string) []Point {
+	lowPoints := []Point{}
+	for r, row := range rows {
+		for c, char := range row {
+			digit := int(char - '0')
+			adjacents := findAdjacents(rows, r, c)
+			if isLow(adjacents, digit) {
+				lowPoints = append(lowPoints, Point{digit, r, c, false})
+			}
+		}
+	}
+	return lowPoints
+}
+
+func findAdjacents(rows []string, r int, c int) []Point {
+	adjacents := []Point{}
+	top := Point{}
+	right := Point{}
+	bottom := Point{}
+	left := Point{}
+	if r != 0 {
+		top = findPoint(rows, r-1, c)
+		top.initialized = true
+	}
+	if c < len(rows[0])-1 {
+		right = findPoint(rows, r, c+1)
+		right.initialized = true
+	}
+	if r < len(rows)-1 {
+		bottom = findPoint(rows, r+1, c)
+		bottom.initialized = true
+	}
+	if c != 0 {
+		left = findPoint(rows, r, c-1)
+		left.initialized = true
+	}
+
+	if top.initialized {
+		adjacents = append(adjacents, top)
+	}
+	if right.initialized {
+		adjacents = append(adjacents, right)
+	}
+	if bottom.initialized {
+		adjacents = append(adjacents, bottom)
+	}
+	if left.initialized {
+		adjacents = append(adjacents, left)
+	}
+	return adjacents
+}
+
+func findPoint(rows []string, r int, c int) Point {
+	for i, row := range rows {
+		if i == r {
+			for j, char := range row {
+				if j == c {
+					return Point{int(char - '0'), r, c, false}
+				}
+			}
+		}
+	}
+	panic("point not found")
+}
+
+func (basin *Basin) calculate(rows []string, lowPoint Point) {
+	for _, adjacent := range findAdjacents(rows, lowPoint.r, lowPoint.c) {
+		if adjacent.digit != 9 && adjacent.digit > lowPoint.digit && !exists(*basin, adjacent) {
+			*basin = append(*basin, adjacent)
+			basin.calculate(rows, adjacent)
+		}
+	}
+}
+
+func exists(points []Point, point Point) bool {
+	for _, p := range points {
+		if p.digit == point.digit && p.r == point.r && p.c == point.c {
+			return true
+		}
+	}
+	return false
 }
 
 func sum(slice []int) int {
@@ -91,9 +143,9 @@ func sum(slice []int) int {
 	return sum
 }
 
-func isLow(slice []int, i int) bool {
-	for _, s := range slice {
-		if i >= s {
+func isLow(points []Point, i int) bool {
+	for _, p := range points {
+		if i >= p.digit {
 			return false
 		}
 	}
