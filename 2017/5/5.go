6754e5eab77ad1a3e7f7cbee1872581b53f86976
Sean Shields <sean.t.shields@gmail.com>
2022-05-30 11:38:57 -0400

    2017 day 5 part 2

--- 2017/5/5.go
+++ 2017/5/5.go
@@ -33,8 +33,23 @@ func solvePart1(input string) int {
 	}
 }
 
-func solvePart2(input string) string {
-	return "Not Implemented"
+func solvePart2(input string) int {
+	steps := 0
+	instructions := toInt(strings.Split(input, "\r\n"))
+	position := 0
+	for {
+		if position < 0 || position >= len(instructions) {
+			return steps
+		}
+		current := position
+		position += instructions[position]
+		if instructions[current] >= 3 {
+			instructions[current]--
+		} else {
+			instructions[current]++
+		}
+		steps++
+	}
 }
 
 func toInt(arr []string) []int {
