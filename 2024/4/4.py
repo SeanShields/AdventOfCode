f8323dd35983f1585a894cb983e28dcb6eff087a
Sean Shields <sean.t.shields@gmail.com>
2024-12-04 18:13:04 -0500

    part 1

--- 2024/4/4.py
+++ 2024/4/4.py
@@ -7,19 +7,16 @@ def solvePart1(fileName):
   total = 0
   for rowIndex, row in enumerate(grid):
     for columnIndex, _ in enumerate(row):
-      if check(rowIndex, columnIndex, "XMAS", grid):
-        total += 1
+      total += check(rowIndex, columnIndex, "XMAS", grid)
   return total
 
 def solvePart2(fileName):
   file = readFile(fileName)
   return file
 
-global textCache
-global rowCache
-global colCache
-
 def check(rowIndex, colIndex, text, grid):
+  total = 0
+  
   textLength = len(text)
   colLength = len(grid[0])
   rowLength = len(grid)
@@ -32,77 +29,112 @@ def check(rowIndex, colIndex, text, grid):
   for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif rowCache == 0:
       break
     rowCache -= 1
+  
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
 
-    # up-right
+  # up-right
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif rowCache == 0 or colCache == colLength - 1:
       break
     rowCache -= 1
     colCache += 1
 
-     # right
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
+
+    # right
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif colCache == colLength - 1:
       break
     colCache += 1
 
-    # down-right
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
+
+  # down-right
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif rowCache == rowLength - 1 or colCache == colLength - 1:
       break
     rowCache += 1
     colCache += 1
 
-    # down
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
+
+  # down
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif rowCache == rowLength - 1:
       break
     rowCache += 1
 
-    # down-left
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
+
+  # down-left
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif rowCache == rowLength - 1 or colCache == 0:
       break
     rowCache += 1
     colCache -= 1
 
-    # left
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
+
+  # left
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
+      total += 1
     elif colCache == 0:
       break
     colCache -= 1
 
-    # up-left
+  textCache = ""
+  rowCache = rowIndex
+  colCache = colIndex
+
+  # up-left
+  for _ in range(textLength):
     textCache += grid[rowCache][colCache]
     if textCache == text:
-      return True
-    elif rowCache == rowLength - 1 or colCache == colLength - 1:
+      total += 1
+    elif rowCache == 0 or colCache == 0:
       break
     rowCache -= 1
     colCache -= 1
 
-  return False
+  return total
 
 def readFile(fileName):
    with open(fileName) as f:
       return f.read()
 
 if __name__ == "__main__":
-    print('Part 1:', solvePart1('test.txt'))
+    print('Part 1:', solvePart1('input.txt'))
     # print('Part 2:', solvePart2('test.txt'))
