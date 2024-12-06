4f36c6ef267299c30dfda726d7a812f0cc033304
Sean Shields <sean.t.shields@gmail.com>
2024-12-06 12:43:49 -0500

    day 5 2024

--- 2024/5/5.py
+++ 2024/5/5.py
@@ -3,7 +3,6 @@ def solvePart1(fileName):
   parts = file.split('\n\n')
   rules = parts[0].split('\n')
   updates = parts[1].split('\n')
-
   total = 0
   for update in updates:
     nums = update.split(',')
@@ -11,25 +10,56 @@ def solvePart1(fileName):
       total += int(nums[round(len(nums) // 2)])
   return total
 
+def solvePart2(fileName):
+  file = readFile(fileName)
+  parts = file.split('\n\n')
+  rules = parts[0].split('\n')
+  updates = parts[1].split('\n')
+  total = 0
+  for update in updates:
+    nums = update.split(',')
+    if not checkOrder(nums, rules):
+      corrected = correctOrder(nums, rules)
+      total += int(corrected[round(len(corrected) // 2)])
+  return total
+
+def correctOrder(nums, rules):
+  for _ in range(len(nums)):
+    for rule in rules:
+      args = rule.split('|')
+      leftIndex = None
+      rightIndex = None
+      try:
+        leftIndex = nums.index(args[0])
+      except:
+        pass
+      try:
+        rightIndex = nums.index(args[1])
+      except:
+          pass
+      if leftIndex is None or rightIndex is None:
+        continue
+      if leftIndex > rightIndex:
+        nums[leftIndex], nums[rightIndex] = nums[rightIndex], nums[leftIndex]
+  return nums
+
 def checkOrder(update, rules):
   for rule in rules:
     args = rule.split('|')
-    if (args[0] not in update and args[1] not in update) \
-      or (args[0] in update and args[1] not in update) \
-      or (args[1] in update and args[0] not in update):
+    left = args[0]
+    right = args[1]
+    if (left not in update and right not in update) \
+      or (left in update and right not in update) \
+      or (right in update and left not in update):
       continue
-    if update.index(args[0]) > update.index(args[1]):
+    if update.index(left) > update.index(right):
       return False
   return True
-
-def solvePart2(fileName):
-  file = readFile(fileName).split('\n')
-  return file
 
 def readFile(fileName):
    with open(fileName) as f:
       return f.read()
 
 if __name__ == "__main__":
-    # print('Part 1:', solvePart1('input.txt'))
-    print('Part 2:', solvePart2('test.txt'))
+    print('Part 1:', solvePart1('input.txt'))
+    print('Part 2:', solvePart2('input.txt'))
